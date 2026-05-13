960770ba0c00e89f345ada5eb98a284b98b61493
santiamm <134665752+santiamm@users.noreply.github.com.>
2026-05-12 22:30:05 -0500

    creacion de carpetas y navegacion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,4 +44,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
