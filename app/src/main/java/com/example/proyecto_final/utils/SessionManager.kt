622a009ad28ce5cf726b27dd1e320526a82df13b
Estiven Rodriguez <estivenalexanderr7@gmail.com>
2026-05-22 04:31:02 -0500

    Agrega SessionManager

---
+++ app/src/main/java/com/example/proyecto_final/utils/SessionManager.kt
@@ -0,0 +1,32 @@
+package com.example.proyecto_final.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SessionManager(context: Context) {
+
+    private val prefs: SharedPreferences =
+        context.getSharedPreferences(
+            "nodo_civico_session",
+            Context.MODE_PRIVATE
+        )
+
+    fun saveLoginSession(email: String) {
+
+        prefs.edit()
+            .putBoolean("IS_LOGGED_IN", true)
+            .putString("USER_EMAIL", email)
+            .apply()
+    }
+
+    fun isLoggedIn(): Boolean {
+        return prefs.getBoolean(
+            "IS_LOGGED_IN",
+            false
+        )
+    }
+
+    fun logout() {
+        prefs.edit().clear().apply()
+    }
+}
