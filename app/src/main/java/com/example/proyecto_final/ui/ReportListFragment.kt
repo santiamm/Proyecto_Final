2225df7cf93cec165c56c4f1df933769aa366c1e
Estiven Rodriguez <estivenalexanderr7@gmail.com>
2026-05-22 05:10:58 -0500

    Actualizacion ReportListFragment

--- app/src/main/java/com/example/proyecto_final/ui/ReportListFragment.kt
+++ app/src/main/java/com/example/proyecto_final/ui/ReportListFragment.kt
@@ -3,21 +3,62 @@ package com.example.proyecto_final.ui
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
-import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyecto_final.NodoCivicoApp
 import com.example.proyecto_final.R
+import com.example.proyecto_final.ui.adapter.ReportAdapter
+import com.example.proyecto_final.viewmodel.AppViewModelFactory
+import com.example.proyecto_final.viewmodel.ReportViewModel
+import kotlinx.coroutines.launch
 
-class ReportListFragment : Fragment(R.layout.fragment_report_list) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+class ReportListFragment :
+    Fragment(R.layout.fragment_report_list) {
+
+    private val viewModel: ReportViewModel by viewModels {
+
+        AppViewModelFactory(
+            (requireActivity().application as NodoCivicoApp).repository
+        )
+    }
+
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?
+    ) {
+
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<ImageButton>(R.id.btnBackLista).setOnClickListener {
+        view.findViewById<ImageButton>(
+            R.id.btnBackLista
+        ).setOnClickListener {
+
             findNavController().navigateUp()
         }
 
-        view.findViewById<CardView>(R.id.cardReporteEjemplo).setOnClickListener {
-            findNavController().navigate(R.id.action_reportListFragment_to_reportDetailFragment)
+        val recyclerView =
+            view.findViewById<RecyclerView>(
+                R.id.recyclerViewReports
+            )
+
+        recyclerView.layoutManager =
+            LinearLayoutManager(requireContext())
+
+        val adapter =
+            ReportAdapter(emptyList())
+
+        recyclerView.adapter = adapter
+
+        viewLifecycleOwner.lifecycleScope.launch {
+
+            viewModel.allReports.collect {
+
+                adapter.updateData(it)
+            }
         }
     }
 }
