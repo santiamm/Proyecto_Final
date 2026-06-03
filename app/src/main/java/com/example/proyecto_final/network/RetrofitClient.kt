bb6c3bff4c7011d9f0e73a83f92a0c056ff339c4
jhon <jhon@m4>
2026-06-03 09:00:39 -0500

    se agrego endpoint de API disponible en internet, no se necesita especificar el puerto

--- app/src/main/java/com/example/proyecto_final/network/RetrofitClient.kt
+++ app/src/main/java/com/example/proyecto_final/network/RetrofitClient.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:5000/"
+    private const val BASE_URL = "https://jhonarzu.pythonanywhere.com/"
 
     private val loggingInterceptor = HttpLoggingInterceptor().apply {
         level = HttpLoggingInterceptor.Level.BASIC
